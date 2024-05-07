699c592c4fd7701154f17ab8f75bfb175bd1d9b9
rmohandasrodrigues <rmohandasrodrigues@gmail.com>
2024-05-06 23:15:16 -0300

    Criado configuração da entidade.

---
+++ src/main/java/com/connectsistemas/todolist/entity/Todo.java
@@ -0,0 +1,17 @@
+package com.connectsistemas.todolist.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+}
