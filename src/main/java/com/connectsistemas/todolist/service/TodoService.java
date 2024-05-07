fb9d84a45a164db33411cc7f89b95feec296d92d
rmohandasrodrigues <rmohandasrodrigues@gmail.com>
2024-05-06 23:39:51 -0300

    Criado o serviço onde fica toda regra de negocio da aplicação.

--- src/main/java/com/connectsistemas/todolist/service/TodoService.java
+++ src/main/java/com/connectsistemas/todolist/service/TodoService.java
@@ -1,8 +1,38 @@
 package com.connectsistemas.todolist.service;
 
+import com.connectsistemas.todolist.entity.Todo;
+import com.connectsistemas.todolist.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+        //Ordenação por maiores prioridades de forma descendente, e ordenação por nome em ordem ascendente.
+        Sort sort = Sort.by("prioridade").descending().and(
+                Sort.by("nome").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
 
 }
